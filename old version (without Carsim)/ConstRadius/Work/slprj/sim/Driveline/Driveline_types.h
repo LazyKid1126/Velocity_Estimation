65ae53905b31be1827b9451693993d85d7c000eb
Lee Jason <88151322+LazyKid1126@users.noreply.github.com>
2024-04-18 05:02:44 +0800

    Add files via upload

---
+++ old version (without Carsim)/ConstRadius/Work/slprj/sim/Driveline/Driveline_types.h
@@ -0,0 +1,11 @@
+#ifndef RTW_HEADER_Driveline_types_h_
+#define RTW_HEADER_Driveline_types_h_
+#ifndef SS_UINT64
+#define SS_UINT64 18
+#endif
+#ifndef SS_INT64
+#define SS_INT64 19
+#endif
+typedef struct mrk1ln3yua_ mrk1ln3yua ; typedef struct gimbih1di0h_
+gimbih1di0h ; typedef struct coyvih0rk1 dwsgrvz41y ;
+#endif
---
+++ old version (without Carsim)/ConstRadius/Work/slprj/sim/PassVeh14DOF/PassVeh14DOF_types.h
@@ -0,0 +1,17 @@
+#ifndef RTW_HEADER_PassVeh14DOF_types_h_
+#define RTW_HEADER_PassVeh14DOF_types_h_
+#ifndef SS_UINT64
+#define SS_UINT64 20
+#endif
+#ifndef SS_INT64
+#define SS_INT64 21
+#endif
+typedef struct b4r3m1sp45_ b4r3m1sp45 ; typedef struct elefnztwqk_ elefnztwqk
+; typedef struct kacurs2jv1_ kacurs2jv1 ; typedef struct hkp5vxtoev_
+hkp5vxtoev ; typedef struct bw4g1ckg0y_ bw4g1ckg0y ; typedef struct
+pelpu4erbs_ pelpu4erbs ; typedef struct ffogqh3wsz_ ffogqh3wsz ; typedef
+struct lo1uh5sdk1_ lo1uh5sdk1 ; typedef struct avgdmhuprh4_ avgdmhuprh4 ;
+typedef struct mh44323250_ mh44323250 ; typedef struct p44mzhiemb_ p44mzhiemb
+; typedef struct avgdmhuprh40_ avgdmhuprh40 ; typedef struct l0o1fxj1vpq_
+l0o1fxj1vpq ; typedef struct mmo2vwjqw3 gwlxzditat ;
+#endif
